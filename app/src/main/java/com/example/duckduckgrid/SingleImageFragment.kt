194c3234485a5790da650bad9ceb42de9eace9af
Oliver Blass <114983621+oblassgit@users.noreply.github.com>
2023-10-19 11:24:52 +0200

    #6 idea add like button to the singleimagefragment (#16)
    * #6 added star button but sharedPref doesnt work yet
    
    * #6 fixed sharedPreferences
    
    * #6 added haptic feedback when clicking the star button
    
    * #6 removed "unliking" haptic
    
    * #6 changed lateinit var to a custom getter for sharedPref
    
    ---------
    
    Co-authored-by: Blass, Oliver <oblass@united-internet.de>

--- app/src/main/java/com/example/duckduckgrid/SingleImageFragment.kt
+++ app/src/main/java/com/example/duckduckgrid/SingleImageFragment.kt
@@ -1,10 +1,15 @@
 package com.example.duckduckgrid
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.HapticFeedbackConstants
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
@@ -15,19 +20,31 @@ class SingleImageFragment : Fragment() {
 
     private var _binding: FragmentSingleImageBinding? = null
     private val args: SingleImageFragmentArgs by navArgs()
+    private val sharedPref: SharedPreferences get() = requireActivity().getPreferences(Context.MODE_PRIVATE)
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
 
         _binding = FragmentSingleImageBinding.inflate(inflater, container, false)
+
         val imgUrl = args.imgUrl
         val date = args.date
+
+
+        var isStarred = sharedPref.getBoolean(imgUrl, false)
+        if (isStarred) {
+            binding.starBtn.setImageResource(android.R.drawable.btn_star_big_on)
+        } else {
+            binding.starBtn.setImageResource(android.R.drawable.btn_star_big_off)
+        }
+
 
         Log.d("DateAndUrl", "$imgUrl $date")
         binding.dateTxt.text = date
@@ -36,10 +53,31 @@ class SingleImageFragment : Fragment() {
             .load(imgUrl)
             .into(binding.imageView)
 
+        binding.starBtn.setOnClickListener {
+            if (isStarred) {
+                binding.starBtn.setImageResource(android.R.drawable.btn_star_big_off)
+                isStarred = false
+                saveStarred(isStarred, imgUrl)
+
+            } else {
+                view?.performHapticFeedback(HapticFeedbackConstants.CONFIRM)
+                binding.starBtn.setImageResource(android.R.drawable.btn_star_big_on)
+                isStarred = true
+                saveStarred(isStarred, imgUrl)
+            }
+        }
+
         return binding.root
 
     }
 
+    private fun saveStarred(isStarred: Boolean, imgUrl: String) {
+        sharedPref
+        with(sharedPref.edit()) {
+            putBoolean(imgUrl, isStarred)
+            commit()
+        }
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
