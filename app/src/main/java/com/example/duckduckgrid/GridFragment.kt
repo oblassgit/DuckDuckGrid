3f35b1c897ee272fb9172372841fa4350537b32a
Oliver Blass <114983621+oblassgit@users.noreply.github.com>
2024-04-03 16:09:21 +0200

    29 pinch gesture to change columns in grid view without staggered grid view (#32)
    * #29 scrolling doesn't work
    
    * #29 doesn't work yet
    
    * #29 transition between recyclerviews works with animation now :)
    
    * #29 nicer animation for transitioning between span counts
    
    * #29 removed redundant code and useless classes
    
    * #29 removed debug code
    
    ---------
    
    Co-authored-by: Blass, Oliver <oblass@united-internet.de>

--- app/src/main/java/com/example/duckduckgrid/GridFragment.kt
+++ app/src/main/java/com/example/duckduckgrid/GridFragment.kt
@@ -13,15 +13,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.duckduckgrid.databinding.FragmentGridBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 
 class GridFragment : Fragment(), CoroutineScope by MainScope() {
+    
     private var _binding: FragmentGridBinding? = null
     private val binding get() = _binding!!
 
@@ -31,7 +31,9 @@ class GridFragment : Fragment(), CoroutineScope by MainScope() {
         requireContext().getSharedPreferences("duckduck", Context.MODE_PRIVATE)
     }
 
-    private var spanCount : Int = 2
+    private val SMALL_MAX_SCALE_FACTOR = 1.25f
+
+    private var viewMode: ViewMode = ViewMode.DEFAULT_VIEW_MODE
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreateView(
@@ -44,40 +46,82 @@ class GridFragment : Fragment(), CoroutineScope by MainScope() {
         val recyclerViewAdapter = RecyclerViewAdapter()
 
         val recyclerView: RecyclerView = binding.recyclerView
+        val recyclerViewSmall: RecyclerView = binding.recyclerViewSmall
         recyclerView.adapter = recyclerViewAdapter
+        recyclerViewSmall.adapter = recyclerViewAdapter
 
         val fab: FloatingActionButton = binding.addDuckBtn
 
         recyclerView.layoutManager =
-            StaggeredGridLayoutManager(spanCount, LinearLayoutManager.VERTICAL).apply {
-                gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS
+            GridLayoutManager(activity, 2, GridLayoutManager.VERTICAL, false)
+
+        recyclerViewSmall.layoutManager =
+            GridLayoutManager(activity, 3, GridLayoutManager.VERTICAL, false)
+
+        when (viewMode) {
+            ViewMode.BIG -> {
+                recyclerView.visibility = View.VISIBLE
+                recyclerViewSmall.visibility = View.INVISIBLE
             }
+
+            ViewMode.SMALL -> {
+                recyclerViewSmall.visibility = View.VISIBLE
+                recyclerView.visibility = View.INVISIBLE
+
+            }
+        }
 
         fab.setOnClickListener {
             viewModel.addItem()
         }
 
-        val scaleGestureDetector = ScaleGestureDetector(
-            requireActivity(),
-            object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
-                override fun onScale(detector: ScaleGestureDetector): Boolean {
-                    spanCount = if(detector.scaleFactor < 1) {
-                        3
 
-                    } else {
-                        2
-                    }
-                    binding.recyclerView.layoutManager =
-                        StaggeredGridLayoutManager(spanCount, LinearLayoutManager.VERTICAL).apply {
-                            gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS
-                        }
-                    return super.onScale(detector)
+
+        val listener = object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+
+            override fun onScale(detector: ScaleGestureDetector): Boolean {
+
+
+
+                if (detector.scaleFactor < 1) {
+                    recyclerViewSmall.animate().scaleX(1f).scaleY(1f).alpha(1f).withStartAction {
+                        recyclerView.animate().scaleY(SMALL_MAX_SCALE_FACTOR)
+                            .scaleX(SMALL_MAX_SCALE_FACTOR)
+                            .alpha(0f)
+                            .start()
+                    }.withEndAction { recyclerViewSmall.visibility = View.VISIBLE
+                    }.start()
+
+
+                    viewMode = ViewMode.SMALL
+
+                } else if (detector.scaleFactor > 1) {
+                    recyclerView.animate().scaleX(1f).scaleY(1f).alpha(1f).withStartAction {
+                        recyclerViewSmall.animate().scaleY(SMALL_MAX_SCALE_FACTOR)
+                            .scaleX(SMALL_MAX_SCALE_FACTOR)
+                            .alpha(0f)
+                            .start()
+                    }.withEndAction { recyclerView.visibility = View.VISIBLE
+                    }.start()
+                    viewMode = ViewMode.BIG
                 }
+
+                return super.onScale(detector)
             }
-        )
+
+
+        }
+
+        val scaleGestureDetector = ScaleGestureDetector(requireActivity(), listener)
 
         binding.recyclerView.setOnTouchListener { _, event ->
             scaleGestureDetector.onTouchEvent(event)
+            false
+        }
+
+        binding.recyclerViewSmall.setOnTouchListener { _, event ->
+            scaleGestureDetector.onTouchEvent(event)
+            false
         }
 
         (binding.recyclerView.adapter as? RecyclerViewAdapter)?.setOnDuckClickListener(object :
@@ -104,8 +148,36 @@ class GridFragment : Fragment(), CoroutineScope by MainScope() {
             }
         })
 
+        (binding.recyclerViewSmall.adapter as? RecyclerViewAdapter)?.setOnDuckClickListener(object :
+            RecyclerViewAdapter.OnDuckClickListener {
+            override fun onClick(position: Int, item: Item) {
+
+                if (item.url != null && item.date != null) {
+                    Log.d("DuckDuck", "WOOOHOOO! $position")
+                    findNavController().navigate(
+                        GridFragmentDirections.actionFirstFragmentToSecondFragment(
+                            item.url ?: "",
+                            item.date ?: "",
+                            item
+                        )
+                    )
+                }
+            }
+
+            override fun starDuck(item: Item, shouldStar: Boolean) {
+                DuckRepository.toggleLiked(item, sharedPreferences)
+                item.url?.let {
+                    viewModel.starItem(it, shouldStar)
+                }
+            }
+        })
+
         viewModel.itemList.observe(viewLifecycleOwner) { itemList ->
             (binding.recyclerView.adapter as? RecyclerViewAdapter)?.submitList(itemList)
+        }
+
+        viewModel.itemList.observe(viewLifecycleOwner) { itemList ->
+            (binding.recyclerViewSmall.adapter as? RecyclerViewAdapter)?.submitList(itemList)
         }
 
         viewModel.loadItems()
