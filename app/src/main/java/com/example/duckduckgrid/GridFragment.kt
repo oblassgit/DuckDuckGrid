ad72271a8f4ffe832e02709f6d6b91440e5f028b
Oliver Blass <114983621+oblassgit@users.noreply.github.com>
2024-04-22 14:29:08 +0200

    47 app crashes without internet connection (#48)
    * #47 added swipe to refresh to grid fragment and app is now checking for internet connection before loading items
    
    * #47 changed color of swipe refreshlayout

--- app/src/main/java/com/example/duckduckgrid/GridFragment.kt
+++ app/src/main/java/com/example/duckduckgrid/GridFragment.kt
@@ -1,15 +1,23 @@
 package com.example.duckduckgrid
 
+import android.R
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.SharedPreferences
 import android.content.res.Configuration
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
 import android.util.Log
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.ScaleGestureDetector
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
+import androidx.core.content.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -21,7 +29,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 
 class GridFragment : Fragment(), CoroutineScope by MainScope() {
-    
+
     private var _binding: FragmentGridBinding? = null
     private val binding get() = _binding!!
 
@@ -35,7 +43,6 @@ class GridFragment : Fragment(), CoroutineScope by MainScope() {
 
     private var viewMode: ViewMode = ViewMode.DEFAULT_VIEW_MODE
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         DuckRepository.sharedPreferences = sharedPreferences
         super.onCreate(savedInstanceState)
@@ -48,6 +55,8 @@ class GridFragment : Fragment(), CoroutineScope by MainScope() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentGridBinding.inflate(inflater, container, false)
+        binding.swipeRefreshLayout.setColorSchemeColors(requireContext().getColorFromAttr(R.attr.colorPrimary))
+        binding.swipeRefreshLayout.setProgressBackgroundColorSchemeColor(requireContext().getColorFromAttr(R.attr.colorBackground))
 
         val recyclerViewAdapter = RecyclerViewAdapter()
 
@@ -78,9 +87,8 @@ class GridFragment : Fragment(), CoroutineScope by MainScope() {
         }
 
         fab.setOnClickListener {
-            viewModel.addItem()
+            addItem()
         }
-
 
 
         val listener = object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
@@ -93,7 +101,8 @@ class GridFragment : Fragment(), CoroutineScope by MainScope() {
                             .scaleX(SMALL_MAX_SCALE_FACTOR)
                             .alpha(0f)
                             .start()
-                    }.withEndAction { recyclerViewSmall.visibility = View.VISIBLE
+                    }.withEndAction {
+                        recyclerViewSmall.visibility = View.VISIBLE
                         recyclerViewSmall.bringToFront()
                     }.start()
 
@@ -108,7 +117,8 @@ class GridFragment : Fragment(), CoroutineScope by MainScope() {
                             .scaleX(SMALL_MAX_SCALE_FACTOR)
                             .alpha(0f)
                             .start()
-                    }.withEndAction { recyclerView.visibility = View.VISIBLE
+                    }.withEndAction {
+                        recyclerView.visibility = View.VISIBLE
                         recyclerView.bringToFront()
                     }.start()
                     viewMode = ViewMode.BIG
@@ -193,7 +203,12 @@ class GridFragment : Fragment(), CoroutineScope by MainScope() {
             (binding.recyclerViewSmall.adapter as? RecyclerViewAdapter)?.submitList(itemList)
         }
 
-        viewModel.loadItems()
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            loadItems()
+            binding.swipeRefreshLayout.isRefreshing = false
+        }
+
+        loadItems()
 
         return binding.root
     }
@@ -201,7 +216,7 @@ class GridFragment : Fragment(), CoroutineScope by MainScope() {
 
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
-        viewModel.loadItems()
+        loadItems()
     }
 
     override fun onAttach(context: Context) {
@@ -215,5 +230,58 @@ class GridFragment : Fragment(), CoroutineScope by MainScope() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun isNetworkAvailable(): Boolean {
+        val conectivityManager: ConnectivityManager = context?.getSystemService()!!
+        return conectivityManager.getNetworkCapabilities(conectivityManager.activeNetwork)
+            .isNetworkCapabilitiesValid()
+    }
+
+    private fun NetworkCapabilities?.isNetworkCapabilitiesValid(): Boolean = when {
+        this == null -> false
+        hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
+                hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED) &&
+                (hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                        hasTransport(NetworkCapabilities.TRANSPORT_VPN) ||
+                        hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                        hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) -> true
+
+        else -> false
+    }
+
+    private fun loadItems() {
+        if (!isNetworkAvailable()) {
+            Toast.makeText(
+                requireContext(),
+                "No active Internet connection available",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            viewModel.loadItems()
+        }
+    }
+
+    private fun addItem() {
+        if (!isNetworkAvailable()) {
+            Toast.makeText(
+                requireContext(),
+                "No active Internet connection available",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            viewModel.addItem()
+        }
+
+    }
+
+    @ColorInt
+    fun Context.getColorFromAttr(
+        @AttrRes attrColor: Int,
+        typedValue: TypedValue = TypedValue(),
+        resolveRefs: Boolean = true
+    ): Int {
+        theme.resolveAttribute(attrColor, typedValue, resolveRefs)
+        return typedValue.data
     }
 }
