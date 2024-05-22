d016b347f089529fe1a7cb02c0040d293b34c025
Oliver Blass <114983621+oblassgit@users.noreply.github.com>
2024-05-22 15:29:50 +0200

    added dividers without any logic

--- app/src/main/java/com/example/duckduckgrid/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/duckduckgrid/RecyclerViewAdapter.kt
@@ -8,16 +8,19 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.bumptech.glide.Glide
 import com.example.duckduckgrid.databinding.GridItemBinding
+import com.example.duckduckgrid.databinding.TimestampSeperatorBinding
 
 
 class RecyclerViewAdapter :
-    ListAdapter<Item, RecyclerViewAdapter.ViewHolder>(object : DiffUtil.ItemCallback<Item>() {
+    ListAdapter<Item, ViewHolder>(object : DiffUtil.ItemCallback<Item>() {
         override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean {
             return oldItem.id == newItem.id
         }
@@ -29,8 +32,8 @@ class RecyclerViewAdapter :
 
     private var onClickListener: OnDuckClickListener? = null
 
-    class ViewHolder(binding: GridItemBinding, val context: Context) :
-        RecyclerView.ViewHolder(binding.root) {
+    class ImageViewHolder(binding: GridItemBinding, val context: Context) :
+        ViewHolder(binding.root) {
         val imgView: ImageView
         val starBtnOff: ImageButton
         val starBtnOn: ImageButton
@@ -46,11 +49,41 @@ class RecyclerViewAdapter :
 
         }
 
+    class TextViewHolder(binding: TimestampSeperatorBinding, val context: Context) :
+        ViewHolder(binding.root) {
+        val textView: TextView
+        val binding: TimestampSeperatorBinding
+
+        init {
+            // Define click listener for the ViewHolder's View
+            textView = binding.timeStampTextView
+            this.binding = binding
+        }
+
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if(position % 6 == 0) {
+            return -1
+        }
+        return super.getItemViewType(position)
+    }
+
 
 
     // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         // Create a new view, which defines the UI of the list item
+
+        if (viewType == -1) {
+            val binding = TimestampSeperatorBinding.inflate(
+                LayoutInflater.from(viewGroup.context),
+                viewGroup,
+                false)
+
+            return TextViewHolder(binding, viewGroup.context)
+        }
+
 
         val binding: GridItemBinding = GridItemBinding.inflate(
             LayoutInflater.from(viewGroup.context),
@@ -58,51 +91,79 @@ class RecyclerViewAdapter :
             false
         )
 
-        return ViewHolder(binding, viewGroup.context)
+        return ImageViewHolder(binding, viewGroup.context)
     }
 
     // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         val item = getItem(position)
-        Glide.with(viewHolder.imgView.context)
-            .load(item.url)
-            .into(viewHolder.imgView)
+        if(viewHolder is ImageViewHolder) {
+            Glide.with(viewHolder.imgView.context)
+                .load(item.url)
+                .into(viewHolder.imgView)
 
 
 
-        viewHolder.itemView.setOnClickListener {
-            onClickListener?.onClick(position, item)
-        }
-        viewHolder.imgView.setOnLongClickListener {
-            Toast.makeText(viewHolder.context, viewHolder.context.getString(R.string.toast_url_saved_to_clipboard), Toast.LENGTH_SHORT).show()
-            viewHolder.itemView.performHapticFeedback(HapticFeedbackConstants.CONTEXT_CLICK)
-            val clip = ClipData.newPlainText("img url", item.url)
-            (viewHolder.context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager).setPrimaryClip(clip)
-            true
-        }
-        viewHolder.imgView.setOnClickListener {
-            onClickListener?.onClick(position, item)
-        }
-        var isStarred = item.liked
-        item.liked = isStarred
-        viewHolder.itemBinding.item = item
-
-
-        //2 onclick listeners because of switching between two buttons to achieve different button images
-        viewHolder.starBtnOff.setOnClickListener {
-            item?.let {
-                onClickListener?.starDuck(item, true)
+            viewHolder.itemView.setOnClickListener {
+                onClickListener?.onClick(position, item)
             }
-            viewHolder.itemBinding.item = item
-            viewHolder.itemView.performHapticFeedback(HapticFeedbackConstants.CONFIRM)
-        }
-
-        viewHolder.starBtnOn.setOnClickListener {
-            //DuckRepository.toggleLiked(item, sharedPref)
-            item?.let {
-                onClickListener?.starDuck(it, false)
+            viewHolder.imgView.setOnLongClickListener {
+                Toast.makeText(
+                    viewHolder.context,
+                    viewHolder.context.getString(R.string.toast_url_saved_to_clipboard),
+                    Toast.LENGTH_SHORT
+                ).show()
+                viewHolder.itemView.performHapticFeedback(HapticFeedbackConstants.CONTEXT_CLICK)
+                val clip = ClipData.newPlainText("img url", item.url)
+                (viewHolder.context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager).setPrimaryClip(
+                    clip
+                )
+                true
             }
+            viewHolder.imgView.setOnClickListener {
+                onClickListener?.onClick(position, item)
+            }
+            var isStarred = item.liked
+            item.liked = isStarred
             viewHolder.itemBinding.item = item
+
+
+            //2 onclick listeners because of switching between two buttons to achieve different button images
+            viewHolder.starBtnOff.setOnClickListener {
+                item?.let {
+                    onClickListener?.starDuck(item, true)
+                }
+                viewHolder.itemBinding.item = item
+                viewHolder.itemView.performHapticFeedback(HapticFeedbackConstants.CONFIRM)
+            }
+
+            viewHolder.starBtnOn.setOnClickListener {
+                //DuckRepository.toggleLiked(item, sharedPref)
+                item?.let {
+                    onClickListener?.starDuck(it, false)
+                }
+                viewHolder.itemBinding.item = item
+            }
+            /*val seperatorInt = position % 4 == 0
+            if (seperatorInt) {
+                val layoutParams: StaggeredGridLayoutManager.LayoutParams? =
+                    viewHolder.itemView.layoutParams as StaggeredGridLayoutManager.LayoutParams?
+                layoutParams!!.isFullSpan = true
+            } else {
+                val layoutParams: StaggeredGridLayoutManager.LayoutParams? =
+                    viewHolder.itemView.layoutParams as StaggeredGridLayoutManager.LayoutParams?
+                layoutParams!!.isFullSpan = false
+            }*/
+
+        } else {
+            (viewHolder as TextViewHolder).let { it.textView.text = "22.05.2024" }
+
+            val layoutParams: StaggeredGridLayoutManager.LayoutParams? =
+                viewHolder.itemView.layoutParams as StaggeredGridLayoutManager.LayoutParams?
+            layoutParams!!.isFullSpan = true
+
+
+
         }
 
     }
